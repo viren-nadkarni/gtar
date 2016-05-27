7873ab00580160b8c3b430adb2baa56b026f9b16
Viren Nadkarni <viren@outlook.com>
2016-05-27 10:24:43 +0530

    Add tuning defaults

--- gtar.py
+++ gtar.py
@@ -7,10 +7,27 @@ import time
 import sys
 
 
-def freq(frames, sampling_frequency=44100, frames_per_buffer=1024):
+tunings = {
+        'standard': [
+            ('E', 329.63),
+            ('B', 246.94),
+            ('G', 196.00),
+            ('D', 146.83),
+            ('A', 110.00),
+            ('E', 82.41)
+        ]
+}
 
+audio_channels = 1
+audio_rate = 48000
+audio_frames_per_buffer = 1024
+audio_sampling_delay = 0.25
+
+
+def freq(frames, sampling_frequency=44100, frames_per_buffer=1024):
     # unpack wave and multiply by the window
     frame_array = numpy.array(wave.struct.unpack("%dh" % (len(frames) / 2), frames)) * numpy.blackman(frames_per_buffer) 
+
     # calculate fft and maximum
     fftout = abs(numpy.fft.rfft(frame_array)) ** 2
     maximum = fftout[1:].argmax() + 1
@@ -28,22 +45,22 @@ def freq(frames, sampling_frequency=44100, frames_per_buffer=1024):
 def main():
     audio = pyaudio.PyAudio()
     stream = audio.open(format=pyaudio.paInt16,
-            channels=1,
-            rate=48000, 
+            channels=audio_channels,
+            rate=audio_rate,
             input=True, 
-            frames_per_buffer=1024)
+            frames_per_buffer=audio_frames_per_buffer)
 
     sys.stdout.write('\n')
     while(True):
-        buff = stream.read(1024)
-        sys.stdout.write( '\r' + str(freq(buff, 48000, 1024)) )
+        buff = stream.read(audio_frames_per_buffer)
+        sys.stdout.write( '\r' + str(freq(buff, audio_channels, audio_frames_per_buffer)) )
         sys.stdout.flush()
-        time.sleep(1)
-
+        time.sleep(audio_sampling_delay)
 
     stream.stop_stream()
     stream.close()
     audio.terminate()
+
 
 if __name__ == '__main__':
     main()
